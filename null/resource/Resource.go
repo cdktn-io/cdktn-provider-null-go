854c8d5ad9395918741fee0b4462acf3d133d820
team-cdk-terrain <github-team-cdk-terrain@cdktn.io>
2026-05-07 17:26:44 +0000

    chore(release): v13.0.1

--- null/resource/Resource.go
+++ null/resource/Resource.go
@@ -5,10 +5,10 @@ package resource
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktn-io/cdktn-provider-null-go/null/v12/jsii"
+	_init_ "github.com/cdktn-io/cdktn-provider-null-go/null/v13/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktn-io/cdktn-provider-null-go/null/v12/resource/internal"
+	"github.com/cdktn-io/cdktn-provider-null-go/null/v13/resource/internal"
 	"github.com/open-constructs/cdk-terrain-go/cdktn"
 )
 
@@ -123,6 +123,15 @@ type Resource interface {
 	// Adds this resource to the terraform JSON output.
 	// Experimental.
 	ToTerraform() interface{}
+	// Applies one or more mixins to this construct.
+	//
+	// Mixins are applied in order. The list of constructs is captured at the
+	// start of the call, so constructs added by a mixin will not be visited.
+	// Use multiple `with()` calls if subsequent mixins should apply to added
+	// constructs.
+	//
+	// Returns: This construct for chaining.
+	With(mixins ...constructs.IMixin) constructs.IConstruct
 }
 
 // The jsii proxy struct for Resource
@@ -870,6 +879,24 @@ func (r *jsiiProxy_Resource) ToTerraform() interface{} {
 		r,
 		"toTerraform",
 		nil, // no parameters
+		&returns,
+	)
+
+	return returns
+}
+
+func (r *jsiiProxy_Resource) With(mixins ...constructs.IMixin) constructs.IConstruct {
+	args := []interface{}{}
+	for _, a := range mixins {
+		args = append(args, a)
+	}
+
+	var returns constructs.IConstruct
+
+	_jsii_.Invoke(
+		r,
+		"with",
+		args,
 		&returns,
 	)
 
--- null/resource/main.go
+++ null/resource/main.go
@@ -59,6 +59,7 @@ func init() {
 			_jsii_.MemberMethod{JsiiMethod: "toTerraform", GoMethod: "ToTerraform"},
 			_jsii_.MemberProperty{JsiiProperty: "triggers", GoGetter: "Triggers"},
 			_jsii_.MemberProperty{JsiiProperty: "triggersInput", GoGetter: "TriggersInput"},
+			_jsii_.MemberMethod{JsiiMethod: "with", GoMethod: "With"},
 		},
 		func() interface{} {
 			j := jsiiProxy_Resource{}
